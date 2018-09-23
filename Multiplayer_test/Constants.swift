69609bc939051aad3fc2111077c5bb2a6bc6644d
Shawn Ma <emailme.mx@gmail.com>
2018-09-22 21:44:10 -0400

    Constants added

---
+++ Multiplayer_test/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Multiplayer_test
+//
+//  Created by Shawn Ma on 9/22/18.
+//  Copyright © 2018 Shawn Ma. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import CoreGraphics
+
+var joystickNotificationName = NSNotification.Name("joystickNotificationName")
+let joystickVelocityMultiplier: CGFloat = 0.00005
+
+struct Constants {
+    static let tankScenePath = "Assets.scnassets/Models/Tank.scn"
+}
+
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
+}
